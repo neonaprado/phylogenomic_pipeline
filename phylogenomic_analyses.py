ac17be06cdb06add53a29036e811d38ab75d8355
Neona Prado <pradon@oregonstate.edu>
2026-05-05 10:56:11 -0700

    added pipeline outline comments

---
+++ phylogenomic_analyses.py
@@ -0,0 +1,56 @@
+#import needed modules
+
+
+#list: unaligned fasta files
+
+
+#folder: aligned files
+
+
+#loop through fasta files
+
+
+#new file path for each aligned file
+
+
+#run mafft on each file
+
+ 
+#list: aligned fasta files
+
+
+#loop through aligned files
+
+
+#run iqtree on each alignment
+
+
+#list: tree files
+
+
+#loop through tree files
+
+
+#read tree into python
+
+
+#find outgroup tip
+
+
+#root tree with outgroup
+
+
+#find Bs Cr At tips
+
+
+#test which pair is monophyletic
+
+
+#store topology: each tree
+
+
+#count: each topology
+
+
+#figure for topology counts
+
